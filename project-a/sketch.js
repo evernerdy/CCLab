dd586c5f675eb3a01e3c67333a7228693c7a44f6
evernerdy <m18610050592@163.com>
2025-03-17 18:27:30 +0800

    changed the shape of the buildings, added people

--- project-a/sketch.js
+++ project-a/sketch.js
@@ -17,6 +17,8 @@ let c = 0;
 let mountainShape = 0.01;
 let crazy = 0.01;
 let breakBoundary = 0;
+let Life = 15;
+let move = false;
 
 let buildingStart = 0;
 let buildingWidth = 4;
@@ -25,43 +27,63 @@ let rise1 = 300;
 let rise2 = 300;
 let rise3 = 300;
 let rise4 = 300;
+let rise0 = -500;
 
 let Doom = 0;
 DoomDecay = false;
 function setup() {
-  let canvas = createCanvas(800, 500);
+   let canvas = createCanvas(800, 500);
   canvas.id("p5-canvas");
   canvas.parent("p5-canvas-container");
 }
 
 function draw() {
+  //detecting should the creature go crazy
   if (rise1 <= 0 || rise2 <= 0 || rise3 <= 0 || rise4 <= 0) {
     build = true;
   }
 
+  //the creature go crazy
   if (build == true) {
     a = random(0, 255);
     b = random(0, 255);
     c = random(0, 255);
+    Life += 2;
     crazy = 1;
     breakBoundary = 300;
     mountainShape += 0.001;
     Doom += 1;
+    move = false;
     rise = random(-300, 300);
   }
 
+  // environment, creature, and interaction that build houses
   drawMountains(a, b, c, mountainShape);
 
-  drawBuilding(rise1, rise2, rise3, rise4);
+  drawBackBuilding(rise0);
+
+    drawBuilding(rise1, rise2, rise3, rise4);
 
   drawCreature(c, b, a, crazy, breakBoundary, scaleCreature);
-
+  //moving people
+  if (rise0> 500)
+    {
+      rise0 = -500;
+      move = false;
+    }
+  
+  if (move == true && rise0 <= 700)
+    {
+      rise0+=50;
+    }
   //Destroy the World after enough buildings
   fill(255, Doom);
   rect(0, 0, 800, 500);
 
   //reset the world
   if (Doom >= 300) {
+    Life = 27;
+    rise0 = -500;
     rise1 = 300;
     rise2 = 300;
     rise3 = 300;
@@ -88,7 +110,7 @@ function draw() {
 }
 
 function mousePressed() {
-  if (rise1 > 0 && rise2 > 0 && rise3 > 0 && rise4 > 0) {
+  move = true;
     crazy += 0.0002;
     breakBoundary += 4;
     scaleCreature += 0.015;
@@ -101,17 +123,20 @@ function mousePressed() {
     } else if (mouseX > 600 && mouseX <= 800) {
       rise4 -= 10;
     }
-  }
 }
 
 function drawMountains(Ma, Mb, Mc, Me) {
   background(255);
   strokeWeight(3);
   for (j = 0; j < 12; j++) {
-    for (i = 0; i < 800; i++) {
-      stroke(Ma, Mb, Mc, 9);
+    for (i = 0; i < 800; i += 3) {
       noiseValueM = noise(mountainShape * i + 1000 * j);
       let M = map(noiseValueM, 0, 1, 0, 50 + j * 50);
+      noStroke();
+      fill(255, 255, 255, Life);
+      textSize(15);
+      text("生", i, M);
+      stroke(Ma, Mb, Mc, 36);
       line(i, 500, i, M);
     }
   }
@@ -119,8 +144,8 @@ function drawMountains(Ma, Mb, Mc, Me) {
 
 function drawCreature(Ca, Cb, Cc, movingSpeed, breakBoundary, scaleC) {
   push();
-  let noiseValueA = noise(0.5*movingSpeed * frameCount);
-  let noiseValueB = noise(0.5*movingSpeed * frameCount + 44444);
+  let noiseValueA = noise(0.5 * movingSpeed * frameCount);
+  let noiseValueB = noise(0.5 * movingSpeed * frameCount + 44444);
   faceX = map(noiseValueA, 0, 1, 0, 800);
   faceY = map(noiseValueB, 0, 1, 0, 100 + breakBoundary);
 
@@ -189,57 +214,68 @@ function limb(posX, speedL, acceleration) {
   pop();
   pop();
 }
-
-function drawBuilding(h1, h2, h3, h4) {
+function drawBackBuilding(h5) {
   push();
-  building(h1, 250);
-  building(h2, 400);
-  building(h3, 600);
-  building(h4, 800);
+  translate(0, h5);
+  for (i = 0; i <= 500; i++) {
+    for (j = 50; j <= 800; j += 100) {
+      let randomTall = random (-50, 50);
+      let noiseValueBuildingBackground = noise(i + 2000 * j);
+      let noiseValueBB = map(
+        noiseValueBuildingBackground,
+        0,
+        1,
+        200,
+      -200
+      );
+
+      noStroke();
+      fill (c,b,a, 100);
+      textSize (20);
+      text ("人",j+0.5*noiseValueBB, i-200);
+      
+      
+    }
+  }
   pop();
 }
-
+function drawBuilding(h1, h2, h3, h4) {
+  push();
+  building(h1, 100);
+  building(h2, 300);
+  building(h3, 500);
+  building(h4, 700);
+  pop();
+}
 function building(H, area) {
   push();
   translate(0, H);
-  if (build == true) {
-    if (sin(frameCount) >= 0) {
-      buildingColor = b - 50;
-    } else {
-      buildingColor = 255;
-    }
-  } else buildingColor = b - 50;
-  fill(buildingColor);
-  noStroke();
-  for (i = area - 250; i <= area; i += 50) {
-    buildingStart = 200;
-    let noiseValueHeight = noise(i + 10000);
-    let heightRandom = map(noiseValueHeight, 0, 1, 0, 60);
-    let noiseValueBuilding = noise(i);
-    let noiseValueB = map(noiseValueBuilding, 0, 1, 0, 50);
-    let noiseValueWidth = noise(i + 20000);
-    let widthRandom = map(noiseValueWidth, 0, 1, 0, 30);
-    rect(i + noiseValueB, buildingStart + heightRandom, buildingWidth, 500);
-    rect(
-      i + noiseValueB,
-      buildingStart + 2 * heightRandom,
-      buildingWidth + widthRandom,
-      500
+  for (i = 0; i <= 500; i += 2) {
+    let noiseValueBuildingBackground = noise(i + 100 * area);
+
+    let noiseValueBB = map(
+      noiseValueBuildingBackground,
+      0,
+      1,
+      -30 - 0.2 * i,
+      30 + 0.2 * i
     );
-    rect(
-      i + noiseValueB,
-      buildingStart + 3 * heightRandom,
-      buildingWidth + 2 * widthRandom,
-      500
-    );
-    rect(
-      i + noiseValueB,
-      buildingStart + 7 * heightRandom,
-      buildingWidth + 3 * widthRandom,
-      500
-    );
-    rect(i + noiseValueB, 425 + heightRandom, 50, 100);
-    rect(0, 475, 800, 50);
+
+    stroke(c, b, a, 200);
+    fill(c, b, a, 3);
+    rect(area + noiseValueBB, 150 + i, 0.08 * i, 50);
+    text("爱", area + noiseValueBB, 150 + i);
+
+    rect(area + 1.3 * noiseValueBB, 300 + i, 0.08 * i, 50);
+    
+    //backtower
+    
+  stroke(c, b, a, 100);
+    fill(c, b, a, 3);
+    rect(area+80 + 0.8*noiseValueBB, 150+area/3 + i, 0.08 * i, 50);
+    text("爱", area+80 + 0.8*noiseValueBB, 150+area/3 + i);
+
+    rect(area+80 + noiseValueBB, 300+area/3 + i, 0.08 * i, 50);
   }
   pop();
 }
